736e070c9106c5f1f6a7587812e88e677affaf6f
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-11-11 23:51:14 +0300

    inclass work

--- lectures/lecture_09_web/part2/sale/src/run.py
+++ lectures/lecture_09_web/part2/sale/src/run.py
@@ -5,3 +5,25 @@
 # Вернуть json в котором указать username, age и sale, которая рассчитывается по формуле round(age / 7).
 # Кука при запросе на сервис auth должна быть вида
 # {'session': '<длинный id>', 'technoatom': '<длинный id2>'}, её можно получить из запроса к сервису.
+from flask import Flask, request, jsonify
+import requests
+import json
+
+HOST = '0.0.0.0'
+app = Flask(__name__)
+
+
+@app.route('/sale')
+def sale():
+    cookies = request.cookies
+    resp = requests.get('http://auth:5000/about_me', cookies=cookies)
+    pars_resp = json.loads(resp.text)
+    age = int(pars_resp['age'])
+    user = pars_resp['username']
+    s = round(age/7)
+    return jsonify({'username': user, 'age': age, 'sale': s})
+
+
+if __name__ == '__main__':
+    app.run(host=HOST)
+
